927ef22d7b540f6f7c77d0a922452a38b5383189
Allan Murage <allanmurage125@gmail.com>
2025-11-20 13:31:15 +0300

    first commit

---
+++ careapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from careapp import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('starter/', views.starter, name='starter'),
+]
